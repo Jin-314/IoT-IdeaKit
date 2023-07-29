556c2ff9b91d7f6adf17af2e119ac020c2a178b8
Jin <58265068+Jin-314@users.noreply.github.com>
2023-07-29 16:45:16 +0900

    add ultrasonic sensor

---
+++ kit/Ultrasonic/main.py
@@ -0,0 +1,22 @@
+import time
+from machine import Pin
+
+#ピンを設定
+trig = Pin(15, Pin.OUT)
+echo = Pin(14, Pin.IN)
+
+while True:
+    trig.low()
+    time.sleep_us(2)
+    trig.high()
+    time.sleep_us(10)
+    trig.low()
+    signaloff, signalon = 0, 0
+    while echo.value() == 0:
+        signaloff = time.ticks_us()
+    while echo.value() == 1:
+        signalon = time.ticks_us()
+    timepassed = signalon - signaloff
+    distance = (timepassed * 0.0343) / 2
+    print("dinstance: ",distance,"cm")
+    time.sleep(0.1)
