54238f2ff034829a91d08ea83c1e157a68029e9d
Jin <58265068+Jin-314@users.noreply.github.com>
2023-07-29 19:55:33 +0900

    add oled and more

---
+++ kit/oled/main.py
@@ -0,0 +1,21 @@
+from machine import Pin, SPI
+import ssd1306
+import img_lib
+import framebuf
+
+#spi設定
+spi = SPI( 1,baudrate = 100000, sck = Pin(10), mosi = Pin(11))
+
+
+#pin設定
+cs = Pin(13,Pin.OUT)
+dc = Pin(9,Pin.OUT)
+rst = Pin(8,Pin.OUT)
+
+display = ssd1306.SSD1306_SPI(128, 64, spi, dc, rst, cs)
+fb = framebuf.FrameBuffer(img_lib.techring, 74, 64, framebuf.MONO_HLSB)
+
+#jin_imgを表示
+display.fill(0)
+display.blit(fb, int((128-74) / 2), 0)
+display.show()
