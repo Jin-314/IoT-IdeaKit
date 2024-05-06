329c771de64f8d8f48d7aafc6a2b31b9a16fb791
jin_neuron <yuujinyuujin77@gmail.com>
2024-05-06 18:34:43 +0900

    update v1.1 fix usb

--- kit/src/9Axis/main.py
+++ kit/src/9Axis/main.py
@@ -1,9 +1,14 @@
-from machine import Pin, I2C
+from machine import Pin, I2C, reset
 import time
 import ustruct
-import math
+from fusion import Fusion
 
-i2c = I2C(0, scl=Pin(17), sda=Pin(16), freq=100000)
+led = Pin("LED", Pin.OUT)
+calibrate_sw = Pin(3, Pin.IN)
+reset_sw = Pin(22, Pin.IN)
+
+fuse = Fusion()
+i2c = I2C(0, scl=Pin(9), sda=Pin(8), freq=100000)
 
 # デバイスのアドレスをスキャンします
 addr = i2c.scan()
@@ -12,6 +17,7 @@ print( "address is :" + str(addr) )
 addr_ag = 0x6a
 addr_m = 0x1c
 
+#センサーレジスタの設定(加速度/ジャイロ)
 CTRL_REG1_G = 0x10
 CTRL_REG2_G = 0x11
 CTRL_REG3_G = 0x12
@@ -27,9 +33,10 @@ i2c.writeto_mem(addr_ag, CTRL_REG3_G, b'\x41')  #0100 0001
 i2c.writeto_mem(addr_ag, ORIENT_CFG_G, b'\x40') #0100 0000
 i2c.writeto_mem(addr_ag, CTRL_REG4, b'\x3a')    #0011 1010
 i2c.writeto_mem(addr_ag, CTRL_REG5_XL, b'\x38') #0011 1000
-i2c.writeto_mem(addr_ag, CTRL_REG6_XL, b'\x3c') #0011 1100
+i2c.writeto_mem(addr_ag, CTRL_REG6_XL, b'\x24') #0010 0100
 i2c.writeto_mem(addr_ag, CTRL_REG7_XL, b'\x00') #0000 0000
 
+#センサーレジスタの設定（地磁気）
 CTRL_REG1_M = 0x20
 CTRL_REG2_M = 0x21
 CTRL_REG3_M = 0x22
@@ -37,56 +44,63 @@ CTRL_REG4_M = 0x23
 CTRL_REG5_M = 0x24
 
 i2c.writeto_mem(addr_m, CTRL_REG1_M, b'\x14')  #0001 0100
-i2c.writeto_mem(addr_m, CTRL_REG2_M, b'\x40')  #0100 0000
+i2c.writeto_mem(addr_m, CTRL_REG2_M, b'\x00')  #0000 0000
 i2c.writeto_mem(addr_m, CTRL_REG3_M, b'\x00')  #0000 0000
 i2c.writeto_mem(addr_m, CTRL_REG4_M, b'\x0c')  #0000 1100
 i2c.writeto_mem(addr_m, CTRL_REG5_M, b'\x00')  #0000 0000
 
+accel = 0, 0, 0
+gyro = 0, 0, 0
+mag = 0, 0, 0
+
+def get9DofData(type="mag"):
+    addr = addr_m if type == "mag" else addr_ag
+    memAddr = 0x18 if type == "gyro" else 0x28
+    scale = 32768
+    scale /= 2 if type == "accel" else 245 if type == "gyro" else 4
+    enableBit = 0x01 if type == "accel" else 0x02 if type == "gyro" else 0x08
+
+    status = i2c.readfrom_mem(addr, 0x27, 1)
+    if status and enableBit != 0x00:
+        data = i2c.readfrom_mem(addr, memAddr, 6)
+
+        x = ustruct.unpack("<h", data[0:2])[0]
+        y = ustruct.unpack("<h", data[2:4])[0]
+        z = ustruct.unpack("<h", data[4:6])[0]
+        x /= scale
+        y /= scale
+        z /= scale
+
+        return (x,y,z)
+    return (0,0,0)
+
+print("Calibrating. Press switch when done.")
+fuse.calibrate(get9DofData, calibrate_sw, 20)
+print(fuse.magbias)
+
 while True:
 
-    print("=======================================================")
+    led.value(1)
+
+    #print("=======================================================")
     #statusレジスタのアドレス0x27の下から1ビット目が加速度のデータ更新状態
     status = i2c.readfrom_mem(addr_ag, 0x27, 1)
-    if status and 0x01 != 0x00:
-        #加速度センサのデータの読み込み
-        data = b''
-        for i in range(0x28, 0x2e):
-            data += i2c.readfrom_mem(addr_ag, i, 1)
-        x = ustruct.unpack("<h", data[0:2])[0]
-        y = ustruct.unpack("<h", data[2:4])[0]
-        z = ustruct.unpack("<h", data[4:6])[0]
-        x = x * 0.000244
-        y = y * 0.000244
-        z = z * 0.000244
-        print("AccX:" + str(x) + " AccY:" + str(y) + " AccZ:" + str(z))
-        #姿勢角の算出
-        phi = math.atan(-y/z)
-        thete = math.atan(x/math.sqrt(y*y+z*z))
-        print("phi:" + str(phi) + " thete:" + str(thete))
-    
-    #statusレジスタのアドレス0x27の下から2ビット目がジャイロのデータ更新状態
-    if status and 0x02 != 0x00:
-        data = b''
-        for i in range(0x18, 0x1e):
-            data += i2c.readfrom_mem(addr_ag, i, 1)
-        x = ustruct.unpack("<h", data[0:2])[0]
-        y = ustruct.unpack("<h", data[2:4])[0]
-        z = ustruct.unpack("<h", data[4:6])[0]
-        x = x * 0.00875
-        y = y * 0.00875
-        z = z * 0.00875
-        print("GyrX:" + str(x) + " GyrY:" + str(y) + " GyrZ:" + str(z))
-    
-    status = i2c.readfrom_mem(addr_m, 0x27, 1)
-    #statusレジスタのアドレス0x27の下から4ビット目が磁気のデータ更新状態
-    if status and 0x08 != 0x00:
-        data = i2c.readfrom_mem(addr_m, 0x28, 6)
-        x = ustruct.unpack("<h", data[0:2])[0]
-        y = ustruct.unpack("<h", data[2:4])[0]
-        z = ustruct.unpack("<h", data[4:6])[0]
-        x = x * 0.00043
-        y = y * 0.00043
-        z = z * 0.00043
-        print("MagX:" + str(x) + " MagY:" + str(y) + " MagZ:" + str(z))
 
-    time.sleep(1)
+    accel = get9DofData("accel")
+    gyro = get9DofData("gyro")
+    mag = get9DofData("mag")
+
+    led.value(0)
+
+    #print(str(ax) + "," + str(ay) + "," + str(az) + ","  + str(gx) + "," + str(gy) + "," + str(gz) + ","  + str(mx) + "," + str(my) + "," + str(mz))
+    
+    fuse.update(accel, gyro, mag)
+    print("1," + str(gyro[0]) + "," + str(gyro[1]) + "," + str(gyro[2]))
+    print("2," + str(accel[0]) + "," + str(accel[1]) + "," + str(accel[2]))
+    print("3," + str(mag[0]) + "," + str(mag[1]) + "," + str(mag[2]))
+    print("4," + str(fuse.roll) + "," + str(fuse.pitch) + "," + str(fuse.heading))
+
+    if reset_sw.value() == 1:
+        reset()
+
+    time.sleep_ms(20)
